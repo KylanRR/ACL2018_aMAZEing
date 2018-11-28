4c96f73b03f9d3069b3f39358fc527f1606b6566
ailinseitamet <43601805+ailinseitamet@users.noreply.github.com>
2018-11-28 12:32:47 +0200

    Update Heros.java

--- Heros.java
+++ Heros.java
@@ -1,112 +1,57 @@
+
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.Rectangle;
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import javax.swing.ImageIcon;
-import java.util.function.BooleanSupplier;
 
 
 public class Heros extends Entite {
-	int velX=0, velY=0;
+	public boolean right, left, up, down, attack;
 	int speed=5;
 
     public Heros(int posx, int posy, int nbDegats, int vieActuelle, int vieMax) {
         super(posx, posy, nbDegats, vieActuelle, vieMax);
     }
+
     public Heros(int posx, int posy) {
-    	super(posx, posy);
+        super(posx, posy);
     }
-    
-    int coordX1, coordY1;
-	public boolean checkCollisions(int posxHero, int posyHero) {
-		if (posxHero == coordX1 && posyHero == coordY1) {
-			return true;
-		}
-		return false;
-	}
 	
-	public void draw(Graphics2D g2d) {
-		g2d.drawImage(getHerosImg(), posx, posy, null);	
-	}
-	
-	public Image getHerosImg() {
-		ImageIcon ic=new ImageIcon("heros.png");
+    public Image getHerosImg() {
+		ImageIcon ic=new ImageIcon(new ImageIcon(getClass().getResource("/Heros.png")).getImage().getScaledInstance(35, 35, Image.SCALE_DEFAULT));
 		return ic.getImage();
 	}
-        
-        public void keyPressed(KeyEvent e) {
-		int key=e.getKeyCode();
-		
-		if(key==KeyEvent.VK_W) {
-			velY=-speed;
-		} else if (key==KeyEvent.VK_S) {
-			velY=speed;
-		}else if (key==KeyEvent.VK_A) {
-			velX=-speed;
-		} else if (key==KeyEvent.VK_D) {
-			velX=speed;
-		}
-        }
-        
-        public void keyReleased(KeyEvent e) {
-		int key=e.getKeyCode();
-		
-		if(key==KeyEvent.VK_W) {
-			velY=0;
-		} else if (key==KeyEvent.VK_S) {
-			velY=0;
-		}else if (key==KeyEvent.VK_A) {
-			velX=0;
-		} else if (key==KeyEvent.VK_D) {
-			velX=0;
-		}
-		
-	}
-	
-	public void update() { 
-		posy+=velY;
-		posx+=velX;
-		
-		checkCollisions();
-	}
-	
-	public boolean checkupdate(int posXHero,int posYHero) {
-    		if (posXHero==posx && posYHero==posy) {
-    			return true;
-    		}
-    		return false;
-    	}
-	
-        public Rectangle getBounds() {
-            return new Rectangle(posx,posy,getHerosImg().getWidth(null), getHerosImg().getHeight(null));	
-	}
-	
-        public boolean statusVie(int coordX1, int coordY1) {
-        	// TODO Auto-generated method stub
-      
-        	if((coordX1 != monstre.getPositionX())&& (coordY1 != monstre.getPositionY())) {
-        		return true ;
-        	}
-        	else {
-        		return false;
-        	}
-        	}
-        
-        public void diminutionVie(int coordX1, int coordY1) {
-        	// TODO Auto-generated method stub
-        	
-        	if((coordX1 == monstre.getPositionX())&& (coordY1 == monstre.getPositionY())) {
-        		return vieActuelle-- ;
-        	}
-        	
-        	}
-	
-	 public int attack(int vieActuelleMonstre) { 
-    		if(checkCollisions()==true) {
-    			vieActuelleMonstre=vieActuelleMonstre-30;	
-    		}
-    		return vieActuelleMonstre;
-    	}     
     
+	public void draw(Graphics2D g2d) {
+		g2d.drawImage(getHerosImg(), posx, posy, null);
+		
+	}
+	
+	public void tick() {
+		if(right) {
+			posx+=speed;
+		}
+		if(left) {
+			posx-=speed;
+		}
+		if(up) {
+			posy-=speed;
+		}
+		if(down) {
+			posy+=speed;
+		}
+		if(attack) {
+			MoteurDeJeu.monstre.nbDegats+=1;
+			MoteurDeJeu.heros.nbDegats+=1;
+			MoteurDeJeu.monstre.vieActuelle-=1;
+			MoteurDeJeu.heros.vieActuelle-=1;
+		}
+		
+	}
+	      
+
+		
 }
+	
+    
+
