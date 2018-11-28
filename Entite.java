c7e91286661d48a7849dfff41af752b5ca28f97b
cristianapetreaca <43601830+cristianapetreaca@users.noreply.github.com>
2018-11-28 13:07:36 +0200

    Update Entite.java

--- Entite.java
+++ Entite.java
@@ -1,11 +1,34 @@
+import java.awt.Rectangle;
+import javax.swing.JFrame;
 import java.util.ArrayList;
-
+import java.util.Timer;
+import java.awt.event.ActionEvent;
 public class  Entite {
 	public int posx;
 	public int posy;
 	public int nbDegats;
 	public int vieActuelle;
 	public int vieMax;
+	
+	
+	Monstres monstre=new Monstres(posx, posy);
+	Heros hero=new Heros(posx, posy); 
+	Timer time;
+	boolean collisions=false;
+	
+	public Entite() {
+		monstre=new Monstres(5,10);
+		hero=new Heros(4,9);
+		time=new Timer();
+		time.start();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		monstre.move();
+		hero.move();
+		checkCollisions();
+		repaint();
+	}
 	
 public Entite(int posx, int posy, int nbDegats, int vieActuelle, int vieMax) {
 	this.posx=posx;
@@ -15,12 +38,12 @@ public Entite(int posx, int posy, int nbDegats, int vieActuelle, int vieMax) {
 	this.vieMax=vieMax;
 }
 
-public Entite(int posx2, int posy2) {
-	this.posx=posx;
-	this.posy=posy;
+    public Entite(int posx, int posy) {
+    	this.posx=posx;
+    	this.posy=posy;
 }
 
-    public int getPosx() {
+	public int getPosx() {
         return posx;
     }
 
@@ -60,18 +83,42 @@ public Entite(int posx2, int posy2) {
         this.vieMax = vieMax;
     }
     
-
+    public Rectangle getBounds() {
+        return new Rectangle(posx, posy, 200, 400);
+    }
 	
-    public void checkCollisions() {
-		 ArrayList<Monstre> monstres=GameFrame.getMonstreList();
+	public void collisions() {
+		 ArrayList<Monstres> monstres=GameFrame.getMonstreList();
 		 
 		 for(int i=0; i<monstres.size();i++) {
-			 Monstre tempMonstre=monstres.get(i);
+			 Monstres tempMonstre=monstres.get(i);
 			 if(getBounds().intersects(monstres.get(i).getBounds())) {
 				 GameFrame.removeMonstre(tempMonstre);
 			 }
 		 }
 	}
 
+	
+	public boolean checkCollisions() {
+		Rectangle monstre=Monstres.getBounds();
+		Rectangle hero=Heros.getBounds();
+		if(hero.intersects(monstre)) {
+			collisions=true;
+		else 
+			collisions=false;
+			
+		}
+	}
+	private void repaint() {
+		// TODO Auto-generated method stub
+
+		}
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Collision Detection");
+		frame.setVisible(true);
+		frame.setSize(800, 250);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
 }
 
